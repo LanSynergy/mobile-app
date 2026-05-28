4d89f712e26603a3d8d23a577820c38da76613ad
azrim <mirzaspc@gmail.com>
2026-05-28 20:26:07 +0700

    ci: set progress watch interval to 2 seconds for real-time Telegram updates

--- tool/telegram_notifier.py
+++ tool/telegram_notifier.py
@@ -469,7 +469,7 @@ def progress_watch():
     status_text = os.environ.get('TG_STATUS_TEXT', 'Building...')
     start = time.time()
     bar_len = 20
-    interval = 10  # seconds between updates
+    interval = 2  # seconds between updates
     tick = 0
 
     while True:
